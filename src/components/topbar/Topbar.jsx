e15924d154d66715d424756d5d4306085ac3a038
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-09-19 11:45:46 -0500

    edits-2024/jalg Changed last name on TopBar and resume

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,45 +7,45 @@ import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 
 
 function Topbar({ menuOpen, setMenuOpen }) {
-    return (
-      <div className={"topbar " + (menuOpen && "active")}>
-        <div className="wrapper">
-          <div className="left">
-            <a href="#intro" className="logo">
-              Jefferson Lopez Garcia
-            </a>
-            <div className="itemContainer">
-              <PersonPin className="icon"></PersonPin>
-              <span>(617) 970 5004</span>
-            </div>
-            <div className="itemContainer">
-              <MailOutline className="icon"></MailOutline>
-              <span>jeffersongarcia1599@gmail.com</span>
-            </div>
-            <div className="itemContainer">
-              <PictureAsPdfIcon className="icon"></PictureAsPdfIcon>
-              <a
-                style={{
-                  marginBottom: "4px",
-                  textDecoration: "none",
-                  color: "black",
-                }}
-                href="https://docs.google.com/document/d/1P11sq8ZOTPS9IkUPx37VuAeiGNWaDj-lU8t_u8D4Zgs/edit"
-              >
-                Resume
-              </a>
-            </div>
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Jefferson Jurado Garcia
+          </a>
+          <div className="itemContainer">
+            <PersonPin className="icon"></PersonPin>
+            <span>(617) 970 5004</span>
           </div>
-          <div className="right">
-            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-              <span className="line1"></span>
-              <span className="line2"></span>
-              <span className="line3"></span>
-            </div>
+          <div className="itemContainer">
+            <MailOutline className="icon"></MailOutline>
+            <span>jeffersongarcia1599@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <PictureAsPdfIcon className="icon"></PictureAsPdfIcon>
+            <a
+              style={{
+                marginBottom: "4px",
+                textDecoration: "none",
+                color: "black",
+              }}
+              href="https://docs.google.com/document/d/1_djS_cAiSU_ZvaspgFMzI35GydeRi1l5kfSgZvtVdS0/edit?usp=sharing"
+            >
+              Resume
+            </a>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Topbar
