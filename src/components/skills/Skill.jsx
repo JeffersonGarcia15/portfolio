b29394a43f6570ab2ee6757a9dcce603e70be1d5
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-09-19 12:50:19 -0500

    edits-2024/jalg Updated skills section

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -1,38 +1,49 @@
 import "./skill.scss"
 
 export default function Skill() {
-    return (
-      <div className="skills" id="skills">
-        <h3>List of Skills</h3>
-        <div className="list-of-skills">
-          <i className="devicon-javascript-plain colored"></i>
-          <i className="devicon-html5-plain-wordmark colored"></i>
-          <i className="devicon-css3-plain-wordmark colored"></i>
-          <i class="devicon-sass-original colored"></i>
-          <i className="devicon-nodejs-plain-wordmark colored"></i>
-          <i class="devicon-express-original-wordmark colored"></i>
-          <i class="devicon-sequelize-plain-wordmark colored"></i>
-          <i className="devicon-react-original-wordmark colored"></i>
-          <i className="devicon-redux-original colored"></i>
-          <i className="devicon-python-plain"></i>
-          <i className="devicon-flask-original-wordmark"></i>
-          <i class="devicon-sqlalchemy-original-wordmark colored"></i>
-          <i className="devicon-postgresql-plain-wordmark colored"></i>
-          <i class="devicon-docker-plain-wordmark colored"></i>
-          <i class="devicon-amazonwebservices-plain-wordmark colored"></i>
-          <i className="devicon-heroku-original-wordmark colored"></i>
-          <i class="devicon-materialui-plain colored"></i>
-          <i className="devicon-git-plain-wordmark colored"></i>
-          <i class="devicon-xd-plain colored"></i>
-          <i class="devicon-mocha-plain colored"></i>
-          <i class="devicon-nextjs-original-wordmark colored"></i>
-          <i class="devicon-django-plain-wordmark colored"></i>
-          <i class="devicon-mongodb-plain-wordmark colored"></i>
-          <i class="devicon-go-line colored"></i>
-          <i class="devicon-googlecloud-plain-wordmark colored"></i>
-        </div>
+  return (
+    <div className="skills" id="skills">
+      <h3>List of Skills</h3>
+      <div className="list-of-skills">
+        <i className="devicon-javascript-plain colored"></i>
+        <i class="devicon-python-plain-wordmark colored"></i>
+        <i class="devicon-typescript-plain colored"></i>
+        <i class="devicon-azuresqldatabase-plain colored"></i>
+
+        <i className="devicon-react-original-wordmark colored"></i>
+        <i className="devicon-html5-plain-wordmark colored"></i>
+        <i className="devicon-css3-plain-wordmark colored"></i>
+        <i class="devicon-sass-original colored"></i>
+        <i class="devicon-tailwindcss-plain-wordmark colored"></i>
+        <i class="devicon-nextjs-original-wordmark colored"></i>
+        <i className="devicon-redux-original colored"></i>
+        <i className="devicon-flask-original-wordmark"></i>
+        <i class="devicon-express-original-wordmark colored"></i>
+        <i class="devicon-nestjs-original-wordmark colored"></i>
+        <i class="devicon-django-plain-wordmark colored"></i>
+
+        <i className="devicon-postgresql-plain-wordmark colored"></i>
+        <i class="devicon-supabase-plain-wordmark colored"></i>
+        <i class="devicon-mongodb-plain-wordmark colored"></i>
+        <i class="devicon-sequelize-plain-wordmark colored"></i>
+        <i class="devicon-sqlalchemy-plain-wordmark colored"></i>
+        <i class="devicon-firebase-plain-wordmark colored"></i>
+
+        <i class="devicon-docker-plain-wordmark colored"></i>
+        <i className="devicon-git-plain-wordmark colored"></i>
+        <i class="devicon-postman-plain-wordmark colored"></i>
+        <i class="devicon-cypressio-plain-wordmark colored"></i>
+        <i class="devicon-jest-plain colored"></i>
+        <i class="devicon-playwright-plain colored"></i>
+
+        <i class="devicon-amazonwebservices-plain-wordmark colored"></i>
+        <i class="devicon-apachekafka-original-wordmark colored"></i>
+
+        <i className="devicon-heroku-original-wordmark colored"></i>
+        <i class="devicon-xd-plain colored"></i>
       </div>
-    );
+    </div>
+  );
 }
 
 
